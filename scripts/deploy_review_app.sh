490027874aea840ed20767b21fe8621d39d49949
tifus69 <tifus69@hotmail.com>
2018-11-12 15:59:07 +0100

    new test

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"$TRAVIS_REPO_SLUG\" }" > now-review-app.json
+echo "{ \"alias\": \"$TRAVIS_BRANCH\" }" > now-review-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-review-app.json
