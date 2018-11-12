9add4e9026706e329dc864ce66ff4ea5b0e12724
tifus69 <tifus69@hotmail.com>
2018-11-12 15:44:19 +0100

    customized test url

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,3 @@
-echo "{ \"alias\": \"$TRAVIS_BRANCH$OWNER$PROJECT\" }" > now-review-app.json
+echo "{ \"alias\": \"$TRAVIS_BRANCH$USER\" }" > now-review-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-review-app.json
