eb5141e1bed31291ce715aeec514841d81dedeab
tifus69 <tifus69@hotmail.com>
2018-11-13 09:10:12 +0100

    Livraison continue

--- scripts/deploy_review_app.sh
+++ scripts/deploy_review_app.sh
@@ -1,3 +1,7 @@
 echo "{ \"alias\": \"$TRAVIS_BRANCH-${TRAVIS_REPO_SLUG/\//-}\" }" > now-review-app.json
 now --public --token $NOW_TOKEN
 now alias --token $NOW_TOKEN --local-config now-review-app.json
+
+echo "{ \"alias\": \"staging-${TRAVIS_REPO_SLUG/\//-}\" }" > now-staging-app.json
+now --public --token $NOW_TOKEN
+now alias --token $NOW_TOKEN --local-config now-staging-app.json
