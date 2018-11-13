090a2ad41dfb05a6b5ec6e6aff075ccc0a3829f3
tifus69 <tifus69@hotmail.com>
2018-11-13 15:33:31 +0100

    set travis name

--- scripts/tag_and_push.sh
+++ scripts/tag_and_push.sh
@@ -1,5 +1,5 @@
-git config --global user.email "tifus69@hotmail.com"
-git config --global user.name "tifus69"
+git config --global user.email "build@travis-ci.com"
+git config --global user.name "Travis CI"
 git checkout $TRAVIS_BRANCH
 git remote add github-origin https://tifus69:$GITHUB_API_KEY@github.com/$TRAVIS_REPO_SLUG.git
 
